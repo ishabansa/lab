ffde524d40d386e917d6530fcf82cccfcc29f89b
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-09 17:54:43 +0530

    9 feb 2023 lab

---
+++ DemoObject.java
@@ -0,0 +1,15 @@
+//how to create object in java,show demo using program
+package Lab;
+
+public class DemoObject { //create class
+	//declaring and initializing string & custom input string
+	String name="ISHA";
+	public static void main(String[] args) {
+		//creating object using new keyword
+		DemoObject obj=new DemoObject();
+		//print & display the object
+		System.out.println(obj.name);
+	}
+}
+
+
